040121adcf0afa2034f3f271bc318454d8cf815a
akima <akm2000@gmail.com>
2013-11-13 17:53:50 +0900

    copy LoggerPipe implementation from another application.

--- lib/logger_pipe.rb
+++ lib/logger_pipe.rb
@@ -1,5 +1,11 @@
 require "logger_pipe/version"
 
 module LoggerPipe
-  # Your code goes here...
+  autoload :Runner, "logger_pipe/runner"
+
+  class << self
+    def run(logger, cmd, options = {})
+      Runner.new(logger, cmd, options).execute
+    end
+  end
 end
---
+++ lib/logger_pipe/runner.rb
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+require "logger_pipe"
+
+require "timeout"
+
+module LoggerPipe
+
+  class Runner
+    DEFAULT_TIMEOUT = 60
+
+    attr_accessor :logger, :cmd, :timeout
+
+    def initialize(logger, cmd, options = {})
+      @logger, @cmd = logger, cmd
+      @timeout = options[:timeout]
+    end
+
+    def execute
+      logger.info("executing: #{cmd}")
+      buf = []
+      # systemをタイムアウトさせることはできないので、popenの戻り値を使っています。
+      # see http://docs.ruby-lang.org/ja/2.0.0/class/Timeout.html
+      com, pid = nil, nil
+      begin
+        Timeout.timeout( @timeout || DEFAULT_TIMEOUT ) do
+
+          # popenにブロックを渡さないと$?がnilになってしまうので敢えてブロックで処理しています。
+          com = IO.popen("#{cmd} 2>&1") do |com|
+            pid = com.pid
+            while line = com.gets
+              buf << line
+              logger.info(line.chomp)
+            end
+          end
+          if $?.exitstatus == 0
+            logger.info("%s\n\e[32mSUCCESS: %s\e[0m" % [buf.join.strip, cmd])
+            return buf.join
+          else
+            msg = "\e[31mFAILURE: %s\n%s\e[0m" % [cmd, buf.join.strip]
+            logger.error(msg)
+            raise Error, msg
+          end
+
+        end
+      rescue Timeout::Error => e
+        logger.error("[#{e.class.name} #{e.message}] now killing process #{pid}: #{cmd}")
+        begin
+          Process.kill('SIGINT', pid) if pid
+        rescue Exception => err
+          logger.error("[#{err.class.name}] #{err.message}")
+        end
+        begin
+          Timeout.timeout(10) do
+            result = com.read
+          end
+        rescue Exception => err
+          logger.error("failure to get result [#{err.class.name}] #{err.message}")
+          result = "<failure to get result>"
+        end
+        begin
+          msg = "\e[31mEXECUTION Timeout: %s\e[0m\n%s\n[result]: %s" % [cmd, buf.join.strip, result]
+          logger.error(msg)
+        rescue Exception => err
+          logger.error("[#{err.class.name}] #{err.message}")
+        end
+        raise e
+      end
+    end
+
+  end
+
+end
